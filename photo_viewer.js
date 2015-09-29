ffe358953e0b41fe6eb3079c6b0c6fe707e1df0f
DJ Jagannathan <desjag@gmail.com>
2015-09-29 01:35:25 -0400

    Add thumbnails, more functionality to lightbox.

--- photo_viewer.js
+++ photo_viewer.js
@@ -1,37 +1,55 @@
+// Set API variables
 var flickr_key = "3e3c7312558715a97a1420a72e2d8251";
 var flickr_secret = "0dd531d8540e3a05";
-
 var photoset_id = "72157659166167826";
 var user_id = "135894911@N08";
 
+// Set URL endpoint for API
 var url = "https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=" + flickr_key + "&photoset_id=" + photoset_id + 
-"&user_id=" + user_id + "&extras=url_m&format=json&jsoncallback=?";
+"&user_id=" + user_id + "&extras=url_s,url_m&format=json&jsoncallback=?";
 
-console.log(url);
-
+// Function to control page UI/content
 function displayImages (album) {
 	var i;
 	var images = album.photo;
 
+	for(i = 0; i < images.length; i++) {
+		console.log(images[i]);
+
+		var thumbnail_item = document.createElement("LI");
+		var thumbnail_image = document.createElement("IMG");
+
+		thumbnail_item.id = images[i].id;
+		thumbnail_item.className = "thumbnail";
+		thumbnail_image.src = images[i].url_s;
+
+		thumbnail_item.appendChild(thumbnail_image);
+		document.getElementById("imageThumbnails").appendChild(thumbnail_item);
+	}
+
+	// Set default image to be first in the album
 	currentImage = images[0];
 	document.getElementById("lightboxImage").src = currentImage.url_m;
 	document.getElementById("lightboxTitle").innerHTML = currentImage.title;
 
+	// Add click event to next/previous toggle buttons in lightbox
 	var toggles = document.querySelectorAll("div.toggle");
-
 	[].forEach.call(toggles, function(toggle) {
 		toggle.addEventListener('click', toggleImage, false);
 	});
 
+	// Function which toggles to next or previous image depending on which button is clicked
 	function toggleImage (e) {
 		console.log(e);
 		console.log(images);
+
+		// Setting these variables for cleanliness
 		var index = images.indexOf(currentImage);
-		console.log("index: " + index);
 		var targetId = e.target.id;
 
 		if(targetId == "toggleNext") {
 
+			// If we reach the end of the list, go back to first image
 			if(index < images.length - 1) {
 				currentImage = images[index + 1];
 			}
@@ -48,6 +66,7 @@ function displayImages (album) {
 		}
 		else if(targetId == "togglePrevious") {
 
+			// If we reach first image, go to last image
 			if(index > 0) {
 				currentImage = images[index - 1];
 			}
@@ -62,6 +81,8 @@ function displayImages (album) {
 			console.log("Previous Image!");
 
 		}
+
+		// Just in case
 		else {
 			return;
 		}
@@ -69,13 +90,13 @@ function displayImages (album) {
 	}
 }
 
-// Retrieve JSON object containing set of images to toggle through using AJAX
+// Retrieve JSON object containing set of images to toggle through using AJAX call
 $.ajax({
   url: url,
   dataType: 'jsonp',
   success: function(data){
   	// Pass images to JS to handle display
   	var album = data.photoset;
-  	displayImages(data.photoset);
+  	displayImages(album);
   }
 });
